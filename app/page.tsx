1ad8c0fb1dfa52dc2c84e72ca11a01cd7220afcf
Arshad Yaseen <arshadpyaseen@gmail.com>
2024-05-18 00:24:37 +0530

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,17 @@
-import { BlogPosts } from 'app/components/posts'
+import { BlogPosts } from "app/components/posts";
 
 export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        Arshad Yaseen
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`I'm a Frontend engineer who does backend stuff too. Also into ML.`}
       </p>
       <div className="my-8">
         <BlogPosts />
       </div>
     </section>
-  )
+  );
 }
