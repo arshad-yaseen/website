5b368a9d253a271a604601eb5b55f82cccdb698a
arshad <arshadpyaseen@gmail.com>
2025-10-22 20:41:10 +0530

    chore: update

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -2,7 +2,8 @@ import ThemeToggle from "../providers/theme/theme-toggle";
 
 export default function Header() {
   return (
-    <header className="flex items-center sticky top-0 justify-end px-6 h-(--header-height)">
+    <header className="flex items-center border-b border-b-neutral-300 sticky top-0 justify-between pr-6 pl-8 h-(--header-height)">
+      <h1 className="font-serif text-2xl">yasui.</h1>
       <ThemeToggle />
     </header>
   );
--- src/components/layout/root-layout.tsx
+++ src/components/layout/root-layout.tsx
@@ -3,7 +3,7 @@ import Header from "./header";
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
-    <main className="flex flex-col min-h-screen w-full [--header-height:--spacing(14)]">
+    <main className="flex flex-col min-h-screen w-full [--header-height:--spacing(16)]">
       <Header />
       <div className="h-[calc(100dvh-var(--header-height))] w-full">
         {children}
