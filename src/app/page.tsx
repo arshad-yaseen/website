e501f2e9a68054433efbe951f90ac19c332d1ee5
arshad <arshadpyaseen@gmail.com>
2025-10-23 09:27:55 +0530

    chore: update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <div className="flex h-full w-full flex-col py-28 items-center">
-      <h1 className="text-4xl/14 text-center max-w-4xl font-medium font-serif tracking-tight">
-        Precision-crafted UI components
+      <h1 className="text-4xl/14 text-center max-w-4xl font-medium tracking-tighter">
+        Hand-crafted UI by Arshad Yaseen
       </h1>
       <div className="size-16"></div>
     </div>
--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -3,7 +3,7 @@ import ThemeToggle from "../providers/theme/theme-toggle";
 export default function Header() {
   return (
     <header className="flex items-center border-b border-b-neutral-300 sticky top-0 justify-between pr-6 pl-8 h-(--header-height)">
-      <h1 className="font-serif text-2xl">yasui.</h1>
+      <h1 className="font-serif text-2xl">ui.</h1>
       <ThemeToggle />
     </header>
   );
