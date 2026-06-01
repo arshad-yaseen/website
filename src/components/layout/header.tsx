ebeab4e1c56a1d3ebfcddc52c5427f974b8e3269
arshad <arshadpyaseen@gmail.com>
2026-06-01 19:26:50 +0530

    update

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -3,7 +3,7 @@ import ThemeToggle from "../providers/theme/theme-toggle";
 export default function Header() {
   return (
     <header className="flex items-center sticky top-0 max-w-(--layout-width) mx-auto w-full justify-between h-(--header-height)">
-      <h1 className="text-xl">nasrin</h1>
+      <h1 className="text-2xl">nasrin</h1>
       <div className="flex items-center space-x-1">
         <ThemeToggle />
       </div>
