a6967f1b213382c01801a2086b0cb3efd395948f
arshad <arshadpyaseen@gmail.com>
2026-06-05 16:41:12 +0530

    update

--- src/app/(docs)/layout.tsx
+++ src/app/(docs)/layout.tsx
@@ -3,9 +3,9 @@ import Sidebar from "@/components/layout/sidebar";
 
 export default function DocsLayout({ children }: PropsWithChildren) {
   return (
-    <div className="mx-auto flex h-[calc(100dvh-var(--header-height))] w-full max-w-(--layout-width)">
+    <div className="mx-auto flex w-full max-w-(--layout-width)">
       <Sidebar />
-      <main className="min-w-0 flex-1 overflow-y-auto py-8 md:pl-6">{children}</main>
+      <main className="min-w-0 flex-1 py-8 md:pl-6">{children}</main>
     </div>
   );
 }
